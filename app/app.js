78432ff0d8fe361ca610827ac47c3e93e1facfb6
Brennan Roberts <brennan.r.roberts@gmail.com>
2019-01-02 17:59:32 -0800

    Update ember-a11y and add visual focus indicator

--- app/app.js
+++ app/app.js
@@ -1,9 +1,16 @@
-import Ember from 'ember';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import Ember from "ember";
+import Resolver from "./resolver";
+import loadInitializers from "ember-load-initializers";
+import config from "./config/environment";
 
 let App;
+
+const oldFocus = HTMLElement.focus;
+
+HTMLElement.focus = function() {
+  console.log("focus", this);
+  oldFocus.apply(this, arguments);
+};
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
